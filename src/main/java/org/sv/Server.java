fa40bb4e04195fd0b0c7785d0e480d0e05077e29
Suraj Prajapati <suraj.prajapati@klearnow.com>
2026-05-14 02:25:27 +0530

    make the request handler concurrently

--- src/main/java/org/sv/Server.java
+++ src/main/java/org/sv/Server.java
@@ -1,6 +1,5 @@
 package org.sv;
 
-
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -29,15 +28,27 @@ public class Server {
             handleConnection(clientConnection);
         }
     }
-    private void handleConnection(Socket clientConnection) {
-        try {
-            handler.handleConnection(clientConnection.getInputStream(), clientConnection.getOutputStream());
-        } catch (IOException ignored) {
-        }
+
+    public void addRoute(HttpMethod opCode, String route, RequestRunner runner) {
+        routes.put(opCode.name().concat(route), runner);
     }
+
+    /* this handles the connection between the client and the server
+     it takes the client connection and passes it to the handler
+     this will handle 1 request at a time howerver we can handle multiple requests at a time by using a thread pool
+    */
+
+
+//    private void handleConnection(Socket clientConnection) {
+//        try {
+//            handler.handleConnection(clientConnection.getInputStream(), clientConnection.getOutputStream());
+//        } catch (IOException ignored) {
+//        }
+//    }
+    
     /*
      * Capture each Request / Response lifecycle in a thread
-     * executed on the threadPool.
+     * executed on the threadPool. asynchronously and concurrently using a thread pool to handle multiple requests at a time
      */
     private void handleConnection(Socket clientConnection) {
         Runnable httpRequestRunner = () -> {
@@ -47,10 +58,6 @@ public class Server {
             }
         };
         threadPool.execute(httpRequestRunner);
-    }
-
-    public void addRoute(HttpMethod opCode, String route, RequestRunner runner) {
-        routes.put(opCode.name().concat(route), runner);
     }
 
 }
